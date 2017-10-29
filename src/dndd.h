cd28129bbe22696e826f5b6360e5e435e3527e7a
zezeon <zezeon@msn.com>
2017-10-29 23:00:02 +0900

    +

--- src/dndd.h
+++ src/dndd.h
@@ -11,12 +11,12 @@ protected:
 	virtual void process();
 
 private:
-	void login(), signin(), upload(), index(), if_logged(), search(), mn(), pg(), edit(), add(), new_book();
-	std::string id, level, name, db, table, book, page;
+	void signin(), index(), search(), mn(), pg(), edit(), add(), new_book();
 	std::string search(std::string s), field(std::string s), quote_encode(std::string);
+	std::array<int, 5> allowlevel(std::string table, std::string book);
 	std::vector<std::string> tables();
 	int maxpage(std::string table, std::string book);
-	std::array<int, 5> allowlevel(std::string table, std::string book);
+	std::string id, level, name, db, table, book, page;
 	std::vector<std::string> tmp;
 	std::array<int, 5> allow;
 };
